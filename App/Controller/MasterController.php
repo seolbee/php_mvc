64ca6ad880e84aa51fbafcc1e11c4c748018bbf9
seolbee <tmf0909@daum.net>
2020-02-19 11:58:45 +0900

    민지 바보 멍청이

--- App/Controller/MasterController.php
+++ App/Controller/MasterController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 class MasterController{
-    public function render($page, $active, $data =[]){
+    public function render($page, $data =[]){
         require (__ROOT . "/views/header.php");
         require __ROOT . "/views/". $page . ".php";
         require __ROOT . "/views/footer.php";
